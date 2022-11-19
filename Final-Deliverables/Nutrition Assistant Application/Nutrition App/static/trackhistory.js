a8ab7faef8f8ce597af29fb2ab798bb75269de90
Devaraj142 <113374219+Devaraj142@users.noreply.github.com>
2022-11-19 18:11:45 +0530

    Delete Final-Deliverables/Nutrition Assistant Application/Nutrition App/static directory

--- Final-Deliverables/Nutrition Assistant Application/Nutrition App/static/trackhistory.js
+++
@@ -1,6 +0,0 @@
-var todaydate = new Date();
-var day = todaydate.getDate();
-var month = todaydate.getMonth() + 1;
-var year = todaydate.getFullYear();
-var datestring = day + "." + month + "." + year;
-document.getElementById("date").value = datestring;
